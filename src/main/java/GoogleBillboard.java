a042611bfb0976fabffec95ea83774fd923343e5
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-01-17 12:09:16 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,9 +1,11 @@
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-public String digits = e.substring(2,12);
-public double dNum = Double.parseDouble(digits);
-public void setup()  
-{            
-    for (int i = 12; i <e.length(); i+=10){
+
+
+public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+	public static void main(final String[] args) {
+		System.out.println("Replace this string with your answer!");
+	}
+	for (int i = 12; i <e.length(); i+=10){
     digits = e.substring(i-10,i);
     dNum = Double.parseDouble(digits);
 System.out.println(dNum);
@@ -11,13 +13,10 @@ System.out.println(dNum);
    System.out.println( "prime");
   } else  System.out.println( "not prime");
     }
-}  public void draw()  
-{   
-  //not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
-  boolean True = false;
+
+	//Finish this function
+	public boolean isPrime(double dNum){
+        boolean True = false;
      for (int j = 2; j <= Math.sqrt(dNum); j++){
        if (dNum%j==0)
      True=true;
@@ -25,5 +24,6 @@ public boolean isPrime(double dNum)
    if (True==false)
   return false;
  return true;
+	}
 
- } 
+}
